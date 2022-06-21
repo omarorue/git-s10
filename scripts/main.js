54e607756b7cae23b12c99d6c062b2d70261d4cf
Omar <omarh0@gmx.com>
2022-06-21 15:30:26 -0300

    Asignacion de Tareas

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,7 @@ const menuLst = d.querySelector('nav .menu')
 //Navegacion
 // menuBtn.onclick = () => menuLst.classList.toggle('active')
 menuBtn.addEventListener('click', ()=> menuLst.classList.toggle('active'))
+
+// Mapa de contacto
+
+// Formulario de contacto
