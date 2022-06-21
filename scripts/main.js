d10cc85e4c8748be2ee8f77a437652414fd9d9a4
Omar <omarh0@gmx.com>
2022-06-21 01:31:43 -0300

    Estilos Navegacion Tablets y Desktop

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+//Declaraciones
+const d = document
+const menuBtn = d.querySelector('nav .btn')
+const menuLst = d.querySelector('nav .menu')
+
+//Navegacion
+// menuBtn.onclick = () => menuLst.classList.toggle('active')
+menuBtn.addEventListener('click', ()=> menuLst.classList.toggle('active'))
